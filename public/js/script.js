6761bfffa3e299a0ae312ed15ed84eed040e2b33
Swizec Teller <swizec@swizec.com>
2011-07-14 02:44:58 -0700

    loading a bio to the screen

--- public/js/script.js
+++ public/js/script.js
@@ -5,7 +5,58 @@ $(function(){
 
             now.get_bio(function (bio) {
                 console.log("Your bio: "+bio);
+
+                Bios.add({text: bio});
             });
         });
     });
+});
+
+$(function () {
+    var Bio = window.Bio = Backbone.Model.extend({});
+
+    var BioList = window.BioList = Backbone.Collection.extend({
+        model: Bio,
+        url: '/bios',
+
+        initialize: function () {
+        }
+    });
+
+    var Bios = window.Bios = new BioList;
+
+    var BioView = window.BioView = Backbone.View.extend({
+        tagname: "li",
+        template: $("#bio-template"),
+
+        initialize: function () {
+            _.bindAll(this, "render");
+            this.model.bind('change', this.render);
+            this.model.view = this;
+        },
+
+        render: function () {
+            var $el = $(this.el);
+            $el.html(this.template.tmpl(this.model.toJSON()));
+
+            return $el;
+        }
+    });
+
+    var AppView = window.AppView = Backbone.View.extend({
+        el: $("#main"),
+
+        initialize: function () {
+            _.bindAll(this, "add_bio");
+
+            Bios.bind("add", this.add_bio);
+        },
+
+        add_bio: function (bio) {
+            var view = new BioView({model: bio});
+            this.$("ul").append(view.render());
+        }
+    });
+
+    var App = window.App = new AppView;
 });
